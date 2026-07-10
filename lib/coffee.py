8b6ebfc279fc8dc95f45cf122b543b32f5a7505a
Regan Tosh <toshregan94@gmail.com>
2026-07-11 00:23:59 +0300

    1st attempt

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+   
+    def size(self):
+        return self._size
+
+   
+    def size(self, size):
+        if size in ["Small", "Medium", "Large"]:
+            self._size = size
+        else:
+            print("size must be Small, Medium, or Large")
+
+    def tip(self):
+        print("This coffee is great, here's a tip!")
+        self.price += 1
+
+        
